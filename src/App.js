8d2f6e1dfc433b24bfab5d55eb509f73ce557c6c
TatShen <shenets-97@mail.ru>
2023-06-21 23:01:00 +0300

    chat-bot

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './Components'
+import './main.scss'
+import Templates from "./Components/Templates/templates";
+
+function App() {
+  return (
+    <div className="App">
+     <Templates></Templates>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Templates/templates.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+import './templates.scss'
+
+function Templates(){
+    return(
+        <div className="content">
+            <header className="header">
+                <p> Шапка </p>
+            </header>
+            <main className="main">
+                основной
+            </main>
+
+            <footer className="footer">
+                подвал
+            </footer>
+        </div>
+    )
+}
+
+export default Templates
+
